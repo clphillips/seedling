099633099ea857ef04c30acfa9598c6886ac6c1b
clphillips <cody@phillipsdata.com>
2015-05-06 09:29:40 -0700

    Fixed crc32 on 32-bit system

--- src/Codesleeve/Fixture/KeyGenerators/Crc32.php
+++ src/Codesleeve/Fixture/KeyGenerators/Crc32.php
@@ -9,7 +9,7 @@ class Crc32
      * @var int $max The maximum supported CRC32 value
      */
     private $max;
-    
+
     /**
      * Initialize max key size
      *
@@ -19,12 +19,18 @@ class Crc32
     {
         $this->max = $max;
     }
-    
+
     /**
      * {@inheritdoc}
      */
     public function generateKey($value, $tableName = null)
     {
-        return crc32($tableName . $value) % $this->max;
+        $crc = (float)sprintf('%u', crc32($tableName . $value));
+        // 32-bit system can't handle unsigned mod using %
+        if ($crc > PHP_INT_MAX) {
+            $cycles = floor($crc / $this->max);
+            return (int)($crc - ($cycles * $this->max));
+        }
+        return (int)$crc % $this->max;
     }
 }
